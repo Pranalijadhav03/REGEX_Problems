cc5f3a4da2440497e13bc93f9328c8adb2bdec60
Pranalijadhav03 <jadhav.pranali2403@gmail.com>
2022-05-25 19:44:09 +0530

    Adition: UC1-Enter a Vaild First Name

---
+++ RegexPatternProblems/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RegexPatternProblems;
+using System;
+namespace RegexPatternProblems
+{
+    class program
+    {
+        public static string instr = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registation Problems");
+            Pattern program = new Pattern();
+            Console.WriteLine("-->Select to Program is Executed<--");
+            Console.WriteLine("1.validate First Name : \n Exit");
+            Console.WriteLine("Select Above Number:->");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine(program.ValidateFirstName(instr));
+                    break;
+                case 2:
+                default:
+                    Console.WriteLine("Please Select valid Number");
+                    break;
+            }
+        }
+    }
+}
